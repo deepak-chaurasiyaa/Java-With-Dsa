f587ef621ee378e21a1023bdd15cda49adc34909
Deepak Chaurasiya <dchaurasiya8589@gmail.com>
2024-05-06 12:16:44 +0530

    Data Type in Java

---
+++ DataType.java
@@ -0,0 +1,9 @@
+public class DataType {
+    public static void main(String[] args) {
+        boolean flagTrue = true;
+        System.out.println(flagTrue);
+
+        boolean flagFalse = false;
+        System.out.println(flagFalse);
+    }
+}
